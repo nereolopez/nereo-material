7ebdc2a8919c00bdf8405b660d54d469f4fe2a10
nereo.lopez <nereo.lopez@outlook.com>
2018-11-16 09:02:32 +0100

    Backdrop front layer position to fixed

--- projects/nm/src/lib/backdrop-front-layer.component.ts
+++ projects/nm/src/lib/backdrop-front-layer.component.ts
@@ -3,18 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'nm-backdrop-front-layer',
   template: `
-    <div class="nm-backdrop-front-layer-content">
-      <ng-content select="nm-backdrop-front-layer-subtitle"></ng-content>
-      <ng-content select="nm-backdrop-front-layer-content"></ng-content>
-    </div>
+    <ng-content select="nm-backdrop-front-layer-subtitle"></ng-content>
+    <ng-content select="nm-backdrop-front-layer-content"></ng-content>    
   `,
   styles: [`
     :host{
         border-top-left-radius: 16px;
         border-top-right-radius: 16px;
         bottom: 0;
-        display: flex;
-        position: absolute;
+        height: 100%;
+        position: fixed;
         top: 56px;
         width: 100%;
     }
