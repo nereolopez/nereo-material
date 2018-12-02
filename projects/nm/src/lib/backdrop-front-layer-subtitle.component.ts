c76c63973e708679c09d09de94ce2004688b444b
Nereo Lopez <nereolopez@imac-de-nereo.home>
2018-12-02 17:20:19 +0100

    backdrop-front-layer-subtitle improved with icon and additional action

--- projects/nm/src/lib/backdrop-front-layer-subtitle.component.ts
+++ projects/nm/src/lib/backdrop-front-layer-subtitle.component.ts
@@ -2,12 +2,31 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'nm-backdrop-front-layer-subtitle',
-  template: `<ng-content></ng-content>`,
+  template: `<ng-container>
+              <ng-content select="mat-icon" class="subtitle-icon"></ng-content>
+              <ng-content select="span"></ng-content>
+              <span class="spacer"></span>
+              <ng-content select="button"></ng-content>
+             </ng-container>`,
   styles: [`
     :host{
+      align-items: center;
       display: flex;
+      flex-direction: row;
       margin: 12px 16px;      
-      width: 100%;
+      width: auto;
+    }
+    
+    :host /deep/ mat-icon{
+      margin-right: 8px;
+    }
+
+    :host /deep/ span {
+      font-size: 1.2rem;
+    }
+
+    :host /deep/ .spacer {
+      flex: 1 1 auto;
     }
   `]
 })
