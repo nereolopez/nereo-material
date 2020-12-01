72a20a038e75c618538e50008d67cafbfbf1c7a6
Nereo Lopez <nereo.lopez@cotecna.es>
2020-12-01 08:40:52 +0100

    Backdrop moved to its own folder

--- projects/nm/src/public_api.ts
+++ projects/nm/src/public_api.ts
@@ -10,14 +10,14 @@ export * from './lib/contextual-toolbar/contextual-toolbar.module';
 export * from './lib/banner.component';
 export * from './lib/banner.module';
 
-export * from './lib/backdrop.component';
-export * from './lib/backdrop-back-layer.component';
-export * from './lib/backdrop-back-layer-content.component';
-export * from './lib/backdrop-back-layer-toolbar.component';
-export * from './lib/backdrop-front-layer.component';
-export * from './lib/backdrop-front-layer-subtitle.component';
-export * from './lib/backdrop-front-layer-content.component';
-export * from './lib/backdrop.module';
+export * from './lib/backdrop/backdrop.component';
+export * from './lib/backdrop/backdrop-back-layer.component';
+export * from './lib/backdrop/backdrop-back-layer-content.component';
+export * from './lib/backdrop/backdrop-back-layer-toolbar.component';
+export * from './lib/backdrop/backdrop-front-layer.component';
+export * from './lib/backdrop/backdrop-front-layer-subtitle.component';
+export * from './lib/backdrop/backdrop-front-layer-content.component';
+export * from './lib/backdrop/backdrop.module';
 
 export * from './lib/fab-speed-dial/fab-speed-dial.module';
 export * from './lib/fab-speed-dial/fab-speed-dial.component';
